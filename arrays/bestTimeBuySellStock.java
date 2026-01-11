c0442e4b17a1202b73ee977c195c78704b7e5e59
Divisha Chandralingam <210087233+divishachn@users.noreply.github.com>
2026-01-10 19:11:16 -0500

    Updated solution to Best Time to Buy and Sell Stock

--- arrays/bestTimeBuySellStock.java
+++ arrays/bestTimeBuySellStock.java
@@ -9,11 +9,7 @@ public class bestTimeBuySellStock {
         int profit = 0;
         for (int i = 1; i < prices.length; i++)
         {
-            if (buy > prices[i])
-            {
-                buy = prices[i];
-            }
-
+            buy = Math.min(buy, prices[i]);
             profit = Math.max(profit, prices[i] - buy);
         }
         return profit;
