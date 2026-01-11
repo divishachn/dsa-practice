706d7475ec37c2d9429d9947495bfdb208b0aee1
Divisha Chandralingam <210087233+divishachn@users.noreply.github.com>
2026-01-10 19:09:29 -0500

    Add Best Time to Buy and Sell Stock solution

---
+++ arrays/bestTimeBuySellStock.java
@@ -0,0 +1,24 @@
+public class bestTimeBuySellStock {
+    // LeetCode Easy
+    // Time Complexity: O(n)
+    // Find the smallest buy price and max profit in one for loop. 
+    // Assume the prices array is at least of size one. 
+
+    public int maxProfit(int[] prices){
+        int buy = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++)
+        {
+            if (buy > prices[i])
+            {
+                buy = prices[i];
+            }
+
+            profit = Math.max(profit, prices[i] - buy);
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        System.out.println("BEST TIME TO BUY AND SELL STOCK");
+    }
+}
